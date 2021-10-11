0249468061ec7d335774689f8c1de26b277a052e
suohilvi <tommi.i.ijas@gmail.com>
2021-10-11 10:58:48 +0300

    First commit of dev branch

---
+++ WorldView/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>WorldView</title>
+	<meta name="Tommi Ijäs" content="360 sphere image viewing service">
+    <!--Boostrap 5 associated styles-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--Site specific styles-->
+	<link rel="stylesheet" href="css/pageStyles.css">
+</head>
+
+<body>
+    <?php include "./nav.php";?>
+
+	<div>
+	Well hello there!
+	</div>
+
+    <!--Boostrap 5 script-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+<?php ?>
