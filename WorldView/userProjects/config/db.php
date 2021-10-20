c6c319c385aa5929304a182124c364e6130f6411
suohilvi <90375880+suohilvi@users.noreply.github.com>
2021-10-20 09:48:32 +0300

    Update db.php

--- WorldView/userProjects/config/db.php
+++ WorldView/userProjects/config/db.php
@@ -9,7 +9,7 @@
     }
     
 
-    include('../../../credentials.php');
+    include('../../credentials.php');
     $connection = new mysqli($hostname, $username, $password, 'worldview') or die("Database connection not established.".$connection->connect_error)
 
 ?>
