ac98e6b07372c229c7fab1940099fc7c258604a2
suohilvi <90375880+suohilvi@users.noreply.github.com>
2021-10-25 08:57:28 +0300

    Update nav.php
    De- active -ted index link

--- WorldView/nav.php
+++ WorldView/nav.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul id="nav" class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" aria-current="page" href="index.php">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="profile.php">Profile</a>
